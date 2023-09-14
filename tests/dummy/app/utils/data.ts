b54ce6c5760629c6f9dae5ec0200eb52cb51796e
echus <varvara@echus.co>
2023-09-14 14:28:41 +1000

    Run pretty-quick

--- tests/dummy/app/utils/data.ts
+++ tests/dummy/app/utils/data.ts
@@ -5,20 +5,20 @@ export const DATA_1: Data[] = [
     x: [1, 2, 3, 4],
     y: [10, 15, 13, 17],
     mode: 'markers',
-    type: 'scatter'
+    type: 'scatter',
   },
   {
     x: [2, 3, 4, 5],
     y: [16, 5, 11, 9],
     mode: 'lines',
-    type: 'scatter'
+    type: 'scatter',
   },
   {
     x: [1, 2, 3, 4],
     y: [12, 9, 15, 12],
     mode: 'lines+markers',
-    type: 'scatter'
-  }
+    type: 'scatter',
+  },
 ];
 
 export const DATA_2: Data[] = [
@@ -26,18 +26,18 @@ export const DATA_2: Data[] = [
     x: [1, 2, 3, 4],
     y: [10, 15, 13, 17],
     mode: 'lines+markers',
-    type: 'scatter'
+    type: 'scatter',
   },
   {
     x: [2, 3, 4, 5],
     y: [16, 5, 11, 9],
     mode: 'markers',
-    type: 'scatter'
+    type: 'scatter',
   },
   {
     x: [1, 2, 3, 4],
     y: [12, 9, 15, 12],
     mode: 'lines+markers',
-    type: 'scatter'
-  }
+    type: 'scatter',
+  },
 ];

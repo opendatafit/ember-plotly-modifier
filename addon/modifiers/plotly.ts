2f853691fd3ccee1db577830ee80fa630a6afd20
James Wilmot <jameswilmot2000@gmail.com>
2023-09-14 11:54:45 +1000

    addon plotly modifier: first pass at a working implementation

--- addon/modifiers/plotly.ts
+++ addon/modifiers/plotly.ts
@@ -1,16 +1,57 @@
-import Modifier from 'ember-modifier';
+import Modifier, { ArgsFor, PositionalArgs, NamedArgs } from 'ember-modifier';
+import Owner from '@ember/owner';
+import { registerDestructor } from '@ember/destroyable';
 
-import * as Plotly from 'plotly.js';
+import Plotly, { Data, Layout, Config } from 'plotly.js';
 
 interface PlotlyModifierArgs {
   Args: {
-    Named: {};
+    Named: {
+      data: Data[];
+      layout?: Partial<Layout>;
+      config?: Partial<Config>;
+      isVisible?: boolean;
+    };
     Positional: [];
-  }
+  };
 }
 
 export default class PlotlyModifier extends Modifier<PlotlyModifierArgs> {
-  async modify(element: Element /*, positional, named*/) {
-    console.log(Plotly);
+  private _plotlyDom!: HTMLElement;
+
+  constructor(owner: Owner, args: ArgsFor<PlotlyModifierArgs>) {
+    super(owner, args);
+    registerDestructor(this, this.destructor);
+  }
+
+  destructor() {
+    if (this && this._plotlyDom) {
+      Plotly.purge(this._plotlyDom);
+    }
+  }
+
+  async modify(
+    element: HTMLElement,
+    []: PositionalArgs<PlotlyModifierArgs>,
+    { data, layout, config, isVisible }: NamedArgs<PlotlyModifierArgs>
+  ) {
+    if (!element) {
+      throw new Error('Modifier has no element');
+    }
+
+    if (!data) {
+      throw new Error('No data passed to modifier');
+    }
+
+    if (!this._plotlyDom) {
+      this._plotlyDom = element;
+      await Plotly.newPlot(this._plotlyDom, data, layout, config);
+      console.log(this._plotlyDom);
+    } else {
+      await Plotly.react(this._plotlyDom, data, layout, config);
+    }
+
+    // Plotly.newPlot(graphDiv, data, layout, config)
+    //
   }
 }

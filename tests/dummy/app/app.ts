5b9556a620c8cd280d79bf32a7d2913ddf2a1051
James Wilmot <jameswilmot2000@gmail.com>
2023-09-18 21:42:41 +1000

    app.ts and index.html: moved global definition from index.html to app.ts

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -3,6 +3,8 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
 
+(window as any).global = window;
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
